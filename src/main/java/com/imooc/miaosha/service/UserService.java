2e2ae5afc98cd4c34eb55bbfb8d75f66c6267208
zeou <doodlister@outlook.com>
2020-01-02 01:11:27 +0800

    user根据id缓存

--- src/main/java/com/imooc/miaosha/service/UserService.java
+++ src/main/java/com/imooc/miaosha/service/UserService.java
@@ -1,5 +1,12 @@
 package com.imooc.miaosha.service;
 
+import com.imooc.miaosha.dao.MiaoshaUserDao;
+import com.imooc.miaosha.domain.MiaoshaUser;
+import com.imooc.miaosha.exception.GlobalException;
+import com.imooc.miaosha.redis.MiaoshaUserKey;
+import com.imooc.miaosha.redis.RedisService;
+import com.imooc.miaosha.result.CodeMsg;
+import com.imooc.miaosha.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,27 +16,57 @@ import com.imooc.miaosha.domain.User;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	UserDao userDao;
-	
-	public User getById(int id) {
-		 return userDao.getById(id);
-	}
+	@Autowired
+	MiaoshaUserDao miaoshaUserDao;
+	@Autowired
+	RedisService redisService;
 
+	public MiaoshaUser getById(long id) {
+		//取缓存
+		MiaoshaUser user = redisService.get(MiaoshaUserKey.getById, ""+id, MiaoshaUser.class);
+		if(user != null) {
+			return user;
+		}
+		//取数据库
+		user = miaoshaUserDao.getById(id);
+		if(user != null) {
+			redisService.set(MiaoshaUserKey.getById, ""+id, user);
+		}
+		return user;
+	}
+	public boolean updatePassword(String token, long id, String formPass) {
+		//取user
+		MiaoshaUser user = getById(id);
+		if(user == null) {
+			throw new GlobalException(CodeMsg.MOBILE_NOT_EXIST);
+		}
+		//更新数据库
+		MiaoshaUser toBeUpdate = new MiaoshaUser();
+		toBeUpdate.setId(id);
+		toBeUpdate.setPassword(MD5Util.formPassToDBPass(formPass, user.getSalt()));
+		miaoshaUserDao.update(toBeUpdate);
+		//处理缓存
+		redisService.delete(MiaoshaUserKey.getById, ""+id);
+		user.setPassword(toBeUpdate.getPassword());
+		redisService.set(MiaoshaUserKey.token, token, user);
+		return true;
+	}
 	@Transactional
 	public boolean tx() {
 		User u1= new User();
 		u1.setId(2);
 		u1.setName("2222");
 		userDao.insert(u1);
-		
+
 		User u2= new User();
 		u2.setId(1);
 		u2.setName("11111");
 		userDao.insert(u2);
-		
+
 		return true;
 	}
-	
+
 }
