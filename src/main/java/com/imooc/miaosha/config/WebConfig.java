4991658902dd90afaa10d5277cdaee31623d01d1
zeou <doodlister@outlook.com>
2020-01-03 19:11:55 +0800

    秒杀地址隐藏、接口限流、验证码防刷

--- src/main/java/com/imooc/miaosha/config/WebConfig.java
+++ src/main/java/com/imooc/miaosha/config/WebConfig.java
@@ -2,21 +2,32 @@ package com.imooc.miaosha.config;
 
 import java.util.List;
 
+import com.imooc.miaosha.access.AccessLimitInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebConfig  extends WebMvcConfigurerAdapter{
-	
-	@Autowired
-	UserArgumentResolver userArgumentResolver;
-	
-	@Override
-	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-		argumentResolvers.add(userArgumentResolver);
-	}
-	
-	
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    UserArgumentResolver userArgumentResolver;
+
+    @Autowired
+    AccessLimitInterceptor accessLimitInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+        registry.addInterceptor(accessLimitInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
+    }
+
+
 }
--- src/main/java/com/imooc/miaosha/controller/MiaoshaController.java
+++ src/main/java/com/imooc/miaosha/controller/MiaoshaController.java
@@ -1,5 +1,6 @@
 package com.imooc.miaosha.controller;
 
+import com.imooc.miaosha.access.AccessLimit;
 import com.imooc.miaosha.rabbitmq.MQSender;
 import com.imooc.miaosha.rabbitmq.MiaoshaMessage;
 import com.imooc.miaosha.redis.GoodsKey;
@@ -10,9 +11,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.imooc.miaosha.domain.MiaoshaOrder;
 import com.imooc.miaosha.domain.MiaoshaUser;
@@ -23,8 +22,12 @@ import com.imooc.miaosha.service.MiaoshaService;
 import com.imooc.miaosha.service.MiaoshaUserService;
 import com.imooc.miaosha.service.OrderService;
 import com.imooc.miaosha.vo.GoodsVo;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 
@@ -70,13 +73,14 @@ public class MiaoshaController implements InitializingBean {
         }
     }
 
-    @RequestMapping(value="/reset", method=RequestMethod.GET)
+
+    @RequestMapping(value = "/reset", method = RequestMethod.GET)
     @ResponseBody
     public Result<Boolean> reset(Model model) {
         List<GoodsVo> goodsList = goodsService.listGoodsVo();
-        for(GoodsVo goods : goodsList) {
+        for (GoodsVo goods : goodsList) {
             goods.setStockCount(100);
-            redisService.set(GoodsKey.getMiaoshaGoodsStock, ""+goods.getId(), 100);
+            redisService.set(GoodsKey.getMiaoshaGoodsStock, "" + goods.getId(), 100);
             localOverMap.put(goods.getId(), false);
         }
         redisService.delete(OrderKey.getMiaoshaOrderByUidGid);
@@ -94,23 +98,30 @@ public class MiaoshaController implements InitializingBean {
      * -----------------------
      * 优化之后
      * QPS:6887
+     *
      * @return
      */
-    @RequestMapping(value = "/do_miaosha", method = RequestMethod.POST)
+    @RequestMapping(value = "/{path}/do_miaosha", method = RequestMethod.POST)
     @ResponseBody
     public Result<Integer> miaosha(Model model, MiaoshaUser user,
-                                   @RequestParam("goodsId") long goodsId) {
+                                   @RequestParam("goodsId") long goodsId,
+                                   @PathVariable("path") String path) {
         model.addAttribute("user", user);
         if (user == null) {
             return Result.error(CodeMsg.SESSION_ERROR);
+        }
+        //验证path
+        boolean check = miaoshaService.checkPath(user, goodsId, path);
+        if (!check) {
+            return Result.error(CodeMsg.REQUEST_ILLEGAL);
         }
         if (localOverMap.get(goodsId)) {
             return Result.error(CodeMsg.MIAO_SHA_OVER);
         }
         //预减库存
-        long stock = redisService.decr(GoodsKey.getMiaoshaGoodsStock, ""+goodsId);//10
+        long stock = redisService.decr(GoodsKey.getMiaoshaGoodsStock, "" + goodsId);//10
         if (stock < 0) {
-            localOverMap.put(goodsId,true);
+            localOverMap.put(goodsId, true);
             return Result.error(CodeMsg.MIAO_SHA_OVER);
         }
         //判断是否已经秒杀到了
@@ -144,16 +155,53 @@ public class MiaoshaController implements InitializingBean {
      * orderId：成功
      * -1：秒杀失败
      * 0： 排队中
-     * */
-    @RequestMapping(value="/result", method=RequestMethod.GET)
+     */
+    @RequestMapping(value = "/result", method = RequestMethod.GET)
     @ResponseBody
-    public Result<Long> miaoshaResult(Model model,MiaoshaUser user,
-                                      @RequestParam("goodsId")long goodsId) {
+    public Result<Long> miaoshaResult(Model model, MiaoshaUser user,
+                                      @RequestParam("goodsId") long goodsId) {
         model.addAttribute("user", user);
-        if(user == null) {
+        if (user == null) {
             return Result.error(CodeMsg.SESSION_ERROR);
         }
-        long result  =miaoshaService.getMiaoshaResult(user.getId(), goodsId);
+        long result = miaoshaService.getMiaoshaResult(user.getId(), goodsId);
         return Result.success(result);
+    }
+    @AccessLimit(seconds=5, maxCount=5, needLogin=true)
+    @RequestMapping(value = "/path", method = RequestMethod.GET)
+    @ResponseBody
+    public Result<String> getMiaoshaPath(HttpServletRequest request, MiaoshaUser user,
+                                         @RequestParam("goodsId") long goodsId,
+                                         @RequestParam(value = "verifyCode", defaultValue = "0") int verifyCode) {
+        if (user == null) {
+            return Result.error(CodeMsg.SESSION_ERROR);
+        }
+        boolean check = miaoshaService.checkVerifyCode(user, goodsId, verifyCode);
+        if (!check) {
+            return Result.error(CodeMsg.REQUEST_ILLEGAL);
+        }
+        String path = miaoshaService.createMiaoshaPath(user, goodsId);
+        return Result.success(path);
+    }
+
+    @AccessLimit(seconds=5, maxCount=5, needLogin=true)
+    @RequestMapping(value = "/verifyCode", method = RequestMethod.GET)
+    @ResponseBody
+    public Result<String> getMiaoshaVerifyCod(HttpServletResponse response, MiaoshaUser user,
+                                              @RequestParam("goodsId") long goodsId) {
+        if (user == null) {
+            return Result.error(CodeMsg.SESSION_ERROR);
+        }
+        try {
+            BufferedImage image = miaoshaService.createVerifyCode(user, goodsId);
+            OutputStream out = response.getOutputStream();
+            ImageIO.write(image, "JPEG", out);
+            out.flush();
+            out.close();
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error(CodeMsg.MIAOSHA_FAIL);
+        }
     }
 }
